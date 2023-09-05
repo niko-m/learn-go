c441ea379a4bac341be6a2c64b7dc3241bf81d71
Niko M <goodguyniko@gmail.com>
2023-09-05 22:21:54 +0300

    lissajous homework

--- ch01/lissajous/main.go
+++ ch01/lissajous/main.go
@@ -10,11 +10,12 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xFF, 0x00, 0xFF}, color.RGBA{0xFF, 0x00, 0x00, 0xFF}}
 
 const (
-	whiteIndex = 0
-	blackIndex = 1
+	bgIndex        = 0
+	primaryIndex   = 1
+	secondaryIndex = 2
 )
 
 func main() {
@@ -41,7 +42,7 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(i%2+1))
 		}
 
 		phase += 0.1
